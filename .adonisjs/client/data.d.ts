9932f4bf47c7121745a91cd6cc756805a2d2322c
Caiokl3i <caio.victor.santos12@gmail.com>
2026-07-29 17:31:52 -0400

    feat: create category crud routes

--- .adonisjs/client/data.d.ts
+++ .adonisjs/client/data.d.ts
@@ -5,9 +5,19 @@
 
 /// <reference path="./manifest.d.ts" />
 import type { InferData, InferVariants } from '@adonisjs/core/types/transformers'
+import type CategoryTransformer from '#transformers/category_transformer'
+import type TransactionTransformer from '#transformers/transaction_transformer'
 import type UserTransformer from '#transformers/user_transformer'
 
 export namespace Data {
+  export type Category = InferData<CategoryTransformer>
+  export namespace Category {
+    export type Variants = InferVariants<CategoryTransformer>
+  }
+  export type Transaction = InferData<TransactionTransformer>
+  export namespace Transaction {
+    export type Variants = InferVariants<TransactionTransformer>
+  }
   export type User = InferData<UserTransformer>
   export namespace User {
     export type Variants = InferVariants<UserTransformer>
--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 import { controllers } from '#generated/controllers'
@@ -15,23 +6,33 @@ router.get('/', () => {
   return { hello: 'world' }
 })
 
+
 router
   .group(() => {
-    router
-      .group(() => {
-        router.post('signup', [controllers.NewAccount, 'store'])
-        router.post('login', [controllers.AccessTokens, 'store'])
-      })
-      .prefix('auth')
-      .as('auth')
-
-    router
-      .group(() => {
-        router.get('profile', [controllers.Profile, 'show'])
-        router.post('logout', [controllers.AccessTokens, 'destroy'])
-      })
-      .prefix('account')
-      .as('profile')
-      .use(middleware.auth())
+    router.post('signup', [controllers.NewAccount, 'store'])
+    router.post('login', [controllers.AccessTokens, 'store'])
   })
-  .prefix('/api/v1')
+  .prefix('auth')
+  .as('auth')
+
+router
+  .group(() => {
+    router.get('profile', [controllers.Profile, 'show'])
+    router.post('logout', [controllers.AccessTokens, 'destroy'])
+  })
+  .prefix('account')
+  .as('profile')
+  .use(middleware.auth())
+
+router
+  .group(() => {
+    router.get('/', [controllers.Categories, 'index'])
+    router.get('/:id', [controllers.Categories, 'show'])
+    router.post('/', [controllers.Categories, 'store'])
+    router.put('/:id', [controllers.Categories, 'update'])
+    router.delete('/:id', [controllers.Categories, 'destroy'])
+  })
+  .prefix('categories')
+  .as('categories')
+  .use(middleware.auth())
+
